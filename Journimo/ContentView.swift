ad38100baabe2e4f9889b1a568b04fe4827e5d33
SaarthakTuli <saarthaktuli@gmail.com>
2022-05-22 15:30:54 +0530

    Initial Commit

---
+++ Journimo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Journimo
+//
+//  Created by Saarthak Tuli on 22/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Journimo/JournimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournimoApp.swift
+//  Journimo
+//
+//  Created by Saarthak Tuli on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct JournimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
